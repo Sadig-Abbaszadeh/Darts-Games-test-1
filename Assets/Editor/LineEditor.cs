32044cd955c488ce25add74b219ecf134378f2da
Sadig-Abbaszadeh <abbaszadesadiq@gmail.com>
2020-11-21 17:05:31 +0400

    Initial commit. Added scene view line rendering with start and end points

---
+++ Assets/Editor/LineEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LineDrawer))]
+public class LineEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        LineDrawer line = target as LineDrawer;
+
+        Vector3 start = line.StartingPoint;
+        Vector3 end = line.EndPoint;
+        Handles.color = line.SceneViewLineColor;
+
+        Handles.DrawLine(start, end);
+    }
+}
---
+++ Assets/Scripts/LineDrawer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDrawer : MonoBehaviour
+{
+    [SerializeField]
+    Transform startingPoint, endPoint;
+    [SerializeField]
+    Color sceneViewLineColor;
+
+    public Vector3 StartingPoint => startingPoint == null ? Vector3.zero : startingPoint.position;
+    public Vector3 EndPoint => endPoint == null ? Vector3.zero : endPoint.position;
+    public Color SceneViewLineColor => sceneViewLineColor == null ? Color.white : sceneViewLineColor;
+}
